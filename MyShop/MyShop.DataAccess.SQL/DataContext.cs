5e92165d346ad2aa34816f9dd2aa2c37ce58935e
devmxv <martin@devmxv.me>
2020-08-03 12:46:44 -0500

    Adding Entity Framework

---
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        //---Constructor
+        //---DefaultConnection is going to be looked up
+        //---in the web.config file
+        public DataContext()
+            : base("DefaultConnection")
+        {
+
+        }
+
+        //---Passing the values of the tables needed taken from module
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        
+    }
+}
