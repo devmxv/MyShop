47a8406635da8b84e428ce3ecd5df9966b189265
devmxv <martin@devmxv.me>
2020-08-04 13:42:41 -0500

    Created Basket UI

---
+++ MyShop/MyShop.WebUI/Controllers/BasketController.cs
@@ -0,0 +1,51 @@
+﻿using MyShop.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyShop.WebUI.Controllers
+{
+    public class BasketController : Controller
+    {
+        IBasketService basketService;
+
+        //---Constructor
+        public BasketController(IBasketService BasketService)
+        {
+            this.basketService = BasketService;
+        }
+
+        // GET: Basket
+        public ActionResult Index()
+        {
+            //---List of all basket items
+            var model = basketService.GetBasketItems(this.HttpContext);
+
+            return View(model);
+        }
+
+        public ActionResult AddToBasket(string Id)
+        {
+            basketService.AddToBasket(this.HttpContext, Id);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult RemoveToBasket(string Id)
+        {
+            basketService.RemoveFromBasket(this.HttpContext, Id);
+
+            return RedirectToAction("Index");
+        }
+
+        //---Basket summary
+        public PartialViewResult BasketSummary()
+        {
+            var basketSummary = basketService.GetBasketSummary(this.HttpContext);
+
+            return PartialView(basketSummary);
+        }
+    }
+}
